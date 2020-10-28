0f3e70d4c698687fe77da0d6ad0d23e210fc699b
Nikhil <43577457+KnifeParty12@users.noreply.github.com>
2020-10-28 14:35:22 +0530

    O(1) space optimization
    First store left product in output array then calculate right product and update simultaneously

---
+++ Array/Product_of_Array_Except_Self.pp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> res(n);
+        
+        res[0] = 1;
+        
+        for(int i = 1; i<n; i++){ // store answer of left product
+            res[i] = res[i-1]*arr[i-1];
+        }
+        
+        int right = 1;
+        for(int i = n-1; i>=0; i--){
+            res[i] = res[i]*right;
+            right *= arr[i];
+        }   
+        return res;
+    }
+};
